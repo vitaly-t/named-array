630f036eebf583a6312529e62b3b62cee6b43dbb
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2018-09-12 18:10:37 +0100

    removing typescript

--- src/named-array.js
+++ src/named-array.js
@@ -9,6 +9,17 @@
         Object.prototype.toNamedArray = toNamedArray;
     }
 
+    /**
+     *
+     * @param {} [options]
+     * Property options
+     *
+     * @param {boolean} [options.configurable=true]
+     * @param {boolean} [options.enumerable=false]
+     * @param {boolean} [options.writable=false]
+     *
+     * @returns {Array}
+     */
     function toNamedArray(options) {
         var keys = Object.keys(this);
         var res = new Array(keys.length);
@@ -18,12 +29,25 @@
         return res.addProperties(keys, options);
     }
 
+    /**
+     *
+     * @param props
+     *
+     * @param {} [options]
+     * Property options
+     *
+     * @param {boolean} [options.configurable=true]
+     * @param {boolean} [options.enumerable=false]
+     * @param {boolean} [options.writable=false]
+     *
+     * @returns {Array}
+     */
     function addProperties(props, options) {
         if (!Array.isArray(props)) {
             throw new TypeError('Parameter "props" must be an array.');
         }
         if (props.length > this.length) {
-            throw new TypeError('Number of properties cannot exceed the target array length.');
+            throw new TypeError('Number of properties (' + props.length + ') exceeds the target array length (' + this.length + ')');
         }
         props.forEach(function (p, idx) {
             if (p in this) {
@@ -43,5 +67,6 @@
             }
             Object.defineProperty(this, p, config);
         }, this);
+        return this;
     }
 })();
